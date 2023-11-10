75e58d1a7aa04d79e923adb4e097f7be94a116a9
developerekene <developerekene@gmail.com>
2023-11-10 11:55:57 +0100

    first commit

---
+++ src/ui/AppEntry.tsx
@@ -0,0 +1,18 @@
+import {
+  // StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React from 'react';
+
+const AppEntry: React.FunctionComponent = () => {
+  return (
+    <View>
+      <Text>App Entry</Text>
+    </View>
+  );
+};
+
+export default AppEntry;
+
+// const styles = StyleSheet.create({});
